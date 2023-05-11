8dff0ced9d91e23e70c0fa27a4c12c34232dc09d
Ahmed Ben Salem <37070490+bensalemahmed@users.noreply.github.com>
2023-05-10 21:10:18 -0500

    init project

---
+++ MonoRepoWithSpring/authenticationmicroservice/src/main/java/com/ahmed/authenticationmicroservice/AuthenticationMicroserviceApplication.java
@@ -0,0 +1,45 @@
+package com.ahmed.authenticationmicroservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthenticationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationMicroserviceApplication.class, args);
+	}
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(RoleService roleService, UserService userService) {
+		return args -> {
+			roleService.saveRole(Role.builder().name("ADMIN").build());
+			roleService.saveRole(Role.builder().name("USER").build());
+			userService.saveUser(User.builder().email("ahmed@esprit.tn").password("admin").username("ahbensalam").roles(new ArrayList<>()).build());
+
+		};
+	}
+
+	@Bean
+	public CommandLineRunner init(RoleService roleService, UserService userService) {
+		return args -> {
+			// init roles list
+			roleService.saveRole(Role.builder().name("ADMIN").build());
+			roleService.saveRole(Role.builder().name("USER").build());
+
+			// init users list
+			userService.saveUser(User.builder().email("ahmed@esprit.tn").password("admin").username("ahbensalam").roles(new ArrayList<>()).build());
+		};
+	}
+}
---
+++ MonoRepoWithSpring/authenticationmicroservice/src/main/java/com/ahmed/authenticationmicroservice/Role.java
@@ -0,0 +1,30 @@
+package com.ahmed.authenticationmicroservice;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Role {
+    /**
+     * id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+
+    /**
+     * name
+     */
+    private String name ;
+
+
+}
